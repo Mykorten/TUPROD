080bd50d5a7bec0936b64a7745f5a2990e7b764b
Alexis Falaise <alexis.falaise@external.engie.com>
2024-09-19 18:19:20 +0200

    fix: attempt dvh for page display

--- app/components/page-display.tsx
+++ app/components/page-display.tsx
@@ -1,6 +1,6 @@
 export function PageDisplay({ children, footer }: { children: React.ReactNode, footer: React.ReactNode }) {
 		return (
-			<div className="bg-black h-full overflow-hidden flex flex-col justify-between">
+			<div className="bg-black min-h-screen overflow-hidden flex flex-col justify-between">
 					<div className="flex-grow overflow-hidden">
 							{children}
 					</div>
